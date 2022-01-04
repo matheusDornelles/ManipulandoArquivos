85a39fc0f3a7a9850e7082360b680c82d992b39a
Matheus <mdmaia91@gmail.com>
2022-01-04 20:18:22 -0300

    Add project files.

---
+++ ManipulandoArquivos/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ExemploPOO.Helper
+{
+    public class FiledHelper
+    {
+        public void ListarDiretorios(string Caminho)
+        {
+            var retornoCaminho = Directory.GetDirectories(Caminho);
+
+            foreach (var retorno in retornoCaminho)
+            {
+                System.Console.WriteLine(retorno);
+            }
+        }
+    }
+}
---
+++ ManipulandoArquivos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExemploPOO.Helper;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminho = "C:\\TrabalhandoComArquivos";
+
+            FiledHelper helper = new FiledHelper();
+            helper.ListarDiretorios(caminho);
+        }
+    }
+}
+            
